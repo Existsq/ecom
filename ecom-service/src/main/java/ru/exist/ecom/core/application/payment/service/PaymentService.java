9b982cffa9a7d6cbe73ff38c75d1088fa14bb014
Exist <148970132+Existsq@users.noreply.github.com>
2025-09-28 20:50:35 +0300

    Create payment service
    * refactor: append entity attribute
    
    * chore: create payment service

---
+++ ecom-service/src/main/java/ru/exist/ecom/core/application/payment/service/PaymentService.java
@@ -0,0 +1,55 @@
+package ru.exist.ecom.core.application.payment.service;
+
+import jakarta.transaction.Transactional;
+import java.math.BigDecimal;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.exist.ecom.core.application.exception.OrderNotFoundException;
+import ru.exist.ecom.core.application.exception.PaymentNotFoundException;
+import ru.exist.ecom.core.domain.enums.OrderStatus;
+import ru.exist.ecom.core.domain.enums.PaymentStatus;
+import ru.exist.ecom.core.domain.order.Order;
+import ru.exist.ecom.core.domain.payment.Payment;
+import ru.exist.ecom.core.infrastructure.order.OrderRepository;
+import ru.exist.ecom.core.infrastructure.payment.PaymentRepository;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+
+  private final PaymentRepository paymentRepository;
+  private final OrderRepository orderRepository;
+
+  @Transactional
+  public Payment createPayment(UUID orderId, BigDecimal amount) {
+    Order order =
+        orderRepository
+            .findById(orderId)
+            .orElseThrow(() -> new OrderNotFoundException("Order not found"));
+
+    Payment payment = new Payment();
+    payment.setOrder(order);
+    payment.setAmount(amount);
+    payment.setStatus(PaymentStatus.PENDING);
+
+    return paymentRepository.save(payment);
+  }
+
+  @Transactional
+  public Payment completePayment(UUID paymentId) {
+    Payment payment =
+        paymentRepository
+            .findById(paymentId)
+            .orElseThrow(() -> new PaymentNotFoundException("Payment not found"));
+
+    payment.setStatus(PaymentStatus.COMPLETED);
+    paymentRepository.save(payment);
+
+    Order order = payment.getOrder();
+    order.setStatus(OrderStatus.PAID);
+    orderRepository.save(order);
+
+    return payment;
+  }
+}
